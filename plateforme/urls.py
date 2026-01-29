6879cbf05f1321577b565b06cf28ae6238aa0391
karkouri001 <abdessamadkarkouri68@gmail.com>
2026-01-29 19:57:18 +0100

    Add homepage route

--- plateforme/urls.py
+++ plateforme/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import include, path
 
+
+def accueil(request):
+    return HttpResponse("Bienvenue sur la plateforme.")
+
 urlpatterns = [
+    path('', accueil),
     path('admin/', admin.site.urls),
     path('api/', include('gestion.urls')),
 ]
